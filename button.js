34752e8e0289347fe11e191393658f6e646c9b31
juan-miguel-leonard <leonardjm1992@gmail.com>
2024-05-19 18:06:00 -0300

    version 0.1

---
+++ button.js
@@ -0,0 +1,5 @@
+function openWhatsApp() {
+    var phoneNumber = "1123899025"; // Replace with your WhatsApp phone number
+    var whatsAppUrl = "https://wa.me/" + phoneNumber;
+    window.open(whatsAppUrl, "_blank");
+}
